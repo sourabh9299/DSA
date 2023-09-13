b4a173e3d48ce9d89a8f23c7c0cf78571f771332
sourabh kumar <sourabhkumar987@hotmail.com>
2023-09-14 00:46:46 +0530

    For no. of charachter finder

--- FindCharcter.java
+++ FindCharcter.java
@@ -3,16 +3,19 @@ import java.util.Scanner;
 public class FindCharcter {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("eneter Name");
+        System.out.println("Enter Name");
+        int counter=0;
         String Name = sc.next();
-        System.out.println("enter Charcter");
-        String ch =sc.next();
-        for(int j =0;j<Name.length();j++){
-            String tocheck=Name.charAt(j);
-            if(tocheck==ch){
+        System.out.println("Enter Character");
+        char ch = sc.next().charAt(0); // Read the first character of input as char
 
+        for (int i = 0; i < Name.length(); i++) {
+            char ab = Name.charAt(i); // Get the character at the ith position
+
+            if (ab == ch) {
+                counter=counter+1;
             }
         }
-
+        System.out.println("Your Charachter "+ch+" Comes :"+counter+" Times");
     }
 }
