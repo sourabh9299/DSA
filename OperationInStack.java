ed51f4377486b02f53f81232c21a6487da7e298a
sourabh kumar <sourabhkumar987@hotmail.com>
2023-10-03 22:47:50 +0530

    Stack all Operations added

--- OperationInStack.java
+++ OperationInStack.java
@@ -1,11 +1,12 @@
+import java.util.Scanner;
+
 public class OperationInStack {
-    static int arr[];
-    static int size;
+    static int []arr;
+    static int size =10;
     static int top =-1;
 
-    OperationInStack(int n){
-        arr = new int[n];
-        this.size = n;
+    OperationInStack(){
+        arr = new int[size];
 
     }
 
@@ -13,18 +14,64 @@ public class OperationInStack {
         return top==-1;
     }
 
-    public static void add(int elem){
+    public void add(){
         if(top==size-1){
             System.out.println("Stack is full");
         }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number to insert");
+        int elem = sc.nextInt();
         top= top+1;
         arr[top]=elem;
         System.out.println("elem is inserted"+ arr[top]);
     }
+    public void delete(){
+        if(isEmpty()){
+            System.out.println("Stack is Empty");
+        return;
+        }
+        top--;
+
+    }
+    public void display(){
+        for (int i = 0;i<=top;i++){
+            System.out.println(arr[i]);
+        }
+    }
 
     public static void main(String[] args) {
-        OperationInStack q = new OperationInStack(10);
-        q.add(11);
+        OperationInStack q = new OperationInStack();
+        Scanner sc = new Scanner(System.in);
+        boolean inputs = true;
+        while(inputs) {
+        System.out.println("Press 1-Insertion, 2 Deletion , 3 Display , 4 Exit");
+        int num = sc.nextInt();
+            switch (num) {
+                case 1:
+                    q.add();
+                    break;
+                case 2:
+                    q.delete();
+                    break;
+                case 3:
+                    q.display();
+                    break;
+                case 4:
+                    inputs = false;
+                    break;
+                default:
+                    System.out.println("enter a correct input");
+
+
+            }
+
+        }
+//
+
+
+
+
+
 
     }
 
